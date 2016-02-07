77a22c782bb59e8747efdebcd9d844aeb6d6d319
brandoncha <brandoncha2005@gmail.com>
2016-02-07 18:05:46 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Homeworkone::Application.config.secret_key_base = 'ec168e2aa98921e68ca654cd5006449a9f6e37d64794dc804b10d5db89f93669442757d8bfe5fc9d24cd1fce987f0dbe38c87e3d98886d0641b641398a47b454'
