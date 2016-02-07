ce07aeb41c16aee19d8ac38ff8326f62f9c4c60f
brandoncha <brandoncha2005@gmail.com>
2016-02-07 20:03:51 +0000

    Add css for headline

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Homeworkone::Application.routes.draw do
+  Homeworkone::Application.routes.draw do
   root 'tests#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
